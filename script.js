5b9e8aad5098a5d22a7752520d374cbb621e02fd
pktkh35-kmitl <68070087@kmitl.ac.th>
2025-10-03 10:08:24 +0700

    Done.

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const myinput = document.getElementById("myinput");
+const action = document.getElementById("action");
+const value = document.getElementById("value");
+const account = document.getElementById("account");
+const cash = document.getElementById("cash");
+const transactions = document.getElementById("transactions");
+let count = 1
 
-function add() {
-    myinput.value = Number(myinput.value) + 1
+function addlog(text) {
+    transactions.value = count + text + "\n" + transactions.value
+    count++
 }
+
+function change() {
+    addlog(", Current account balance: " + account.value + ", Current cash balanec: " + cash.value)
+}
+
+function proceed() {
+    if (action.value === "deposit" && (Number(cash.value) >= Number(value.value))) {
+        cash.value = Number(cash.value) - Number(value.value)
+        account.value = Number(account.value) + Number(value.value)
+        addlog(", Current account balance: " + account.value + ", Current cash balanec: " + cash.value)
+        return
+    } else if (action.value === "withdraw" && (Number(account.value) >= Number(value.value))) {
+        account.value = Number(account.value) - Number(value.value)
+        cash.value = Number(cash.value) + Number(value.value)
+        addlog(", Current account balance: " + account.value + ", Current cash balanec: " + cash.value)
+        return
+    }
+
+    addlog(", Couldn't deposit entered balance. (Insufficient cash balance)")
+}
+
+addlog(", Current account balance: " + account.value + ", Current cash balanec: " + cash.value)
